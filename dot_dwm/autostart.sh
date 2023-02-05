6d723eb392c5428870871fb54df6fe97051dd433
t-p-r <quanthangthien1000@gmail.com>
2023-02-05 18:12:27 +0700

    main

---
+++ dot_dwm/autostart.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+while true; do
+    xsetroot -name "🔋 $(cat /sys/class/power_supply/BAT0/capacity)%  🌡 $(sed 's/000$/°C/' /sys/class/thermal/thermal_zone0/temp)  📏 $(free -m | awk '(NR==2){ print $2-$7 }')MB | $(asusctl profile -p | awk '{print $4}') | $(date +"%Y/%m/%d %R") "
+    sleep 1
+done &
+
+picom --config ~/.dwm/picom.conf &
+
+xbindkeys -f /home/tar_palantir/.dwm/.xbindkeysrc &
+
+feh --bg-fill /home/tar_palantir/.dwm/liyue.jpg &
+
+nm-applet &
+xfce4-power-manager &
+ibus-daemon -drxR &
+volctl &
+/usr/lib/polkit-kde-authentication-agent-1 &
+dunst &
+udiskie &
