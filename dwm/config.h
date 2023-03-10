7c1b08bcd7aed8bec32bd34126de26cb6399724f
t-p-r <quanthangthien1000@gmail.com>
2023-03-10 15:45:47 +0700

    main

--- dwm/config.h
+++ dwm/config.h
@@ -13,10 +13,6 @@ static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 8;       /* vertical padding of bar */
 static const int sidepad            = 12;       /* horizontal padding of bar */
-static const int float_x			= 640;	
-static const int float_y			= 320;	/* floating window spawn locations */
-static const int float_w			= 1280;	
-static const int float_h			= 800;	/* floating window sizes */
 static const char *fonts[]          = { "JetBrains Mono Nerd Font:size=11" };
 static const char dmenufont[]       = "JetBrains Mono Nerd Font:size=11";
 static const char col_gray1[]       = "#222222";
